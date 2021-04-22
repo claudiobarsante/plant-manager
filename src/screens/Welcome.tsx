101ba947f8e2ec38cd87ab0284441730cb9f2a7d
Claudio Barsante <clbmribas@gmail.com>
2021-04-21 22:09:23 -0300

    Jost fonts installed

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet,Dimensions } from 'react-native';
+import {
+	SafeAreaView,
+	View,
+	Text,
+	Image,
+	TouchableOpacity,
+	StyleSheet,
+	Dimensions,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import wateringImg from '../assets/watering.png';
-import colors from '../colors';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 export default function Welcome() {
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text style={styles.title}>Manage {'\n'}your plants easily</Text>
-			<Image source={wateringImg} style={styles.image} resizeMode='contain' />
+			<View style={styles.wrapper}>
+				<Text style={styles.title}>Manage {'\n'}your plants easily</Text>
+				<Image source={wateringImg} style={styles.image} resizeMode='contain' />
 
-			<Text style={styles.subtitle}>
-				Don't forget to water your plants anymore. We take care to remind you whenever you need
-			</Text>
-			<TouchableOpacity style={styles.button} activeOpacity={0.7}>
-				<Text style={styles.buttonText}>></Text>
-			</TouchableOpacity>
+				<Text style={styles.subtitle}>
+					Don't forget to water your plants anymore. We take care to remind you whenever you need
+				</Text>
+				<TouchableOpacity style={styles.button} activeOpacity={0.7}>
+					<Text>
+						<Feather name='chevron-right' style={styles.buttonIcon} />
+					</Text>
+				</TouchableOpacity>
+			</View>
 		</SafeAreaView>
 	);
 }
@@ -21,37 +35,43 @@ export default function Welcome() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+	},
+	wrapper: {
+		flex: 1,
 		justifyContent: 'space-around',
 		alignItems: 'center',
+		paddingHorizontal: 20,
 	},
-    image:{        
-        height:Dimensions.get('window').width*0.7
-    },
+	image: {
+		height: Dimensions.get('window').width * 0.7,
+	},
 	button: {
 		backgroundColor: colors.green,
 		justifyContent: 'center',
 		alignItems: 'center',
-        borderRadius:16,
-        marginBottom:10,
-        height:56,
-        width:56
+		borderRadius: 16,
+		marginBottom: 10,
+		height: 56,
+		width: 56,
 	},
-    buttonText:{
-        color:colors.white,
-        fontSize: 24
-
-    },
-	title: {
+	buttonIcon: {
+		color: colors.white,
 		fontSize: 32,
+	},
+	title: {
+		fontSize: 28,
 		fontWeight: 'bold',
 		textAlign: 'center',
 		color: colors.heading,
 		marginTop: 38,
+		fontFamily: fonts.heading,
+		lineHeight: 34,
 	},
 	subtitle: {
 		textAlign: 'center',
 		fontSize: 18,
 		paddingHorizontal: 20,
 		color: colors.heading,
+		fontFamily: fonts.text,
 	},
 });
